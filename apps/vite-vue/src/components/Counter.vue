d1a3b0d945f5afb8f631441ce2659497f1b6617d
sokhuong uon <sokhuong.usk@gmail.com>
2022-04-06 12:09:05 +0700

    🎨 use prettier as default

--- apps/vite-vue/src/components/Counter.vue
+++ apps/vite-vue/src/components/Counter.vue
@@ -4,16 +4,16 @@ import { Button } from '@discord-clone/vue'
 
 const count = ref(0)
 onMounted(() => {
-  console.log(`initial value of count is ${count.value}`);
+	console.log(`initial value of count is ${count.value}`)
 })
 watch(count, (value, oldValue) => {
-  console.log(value);
+	console.log(value)
 })
 const increment = () => {
-  count.value++
+	count.value++
 }
 </script>
 
 <template>
-  <Button class="ring-2 ring-pink-800" @click="increment"></Button>
+	<Button className="ring-2 ring-pink-800" @click="increment"> </Button>
 </template>
--- packages/vue/src/Button.vue
+++ packages/vue/src/Button.vue
@@ -1,3 +1,5 @@
 <template>
-  <button class="w-24 h-24 bg-red-700 rounded-full overflow-clip focus:outline-none">Hello Tailwind</button>
+	<button className="w-24 h-24 bg-red-700 rounded-full overflow-clip focus:outline-none">
+		Hello Tailwind
+	</button>
 </template>

c11f48ecba87c5e68169fbe9097ad99805e3899a
sokhuong uon <sokhuong.usk@gmail.com>
2022-04-11 13:15:46 +0700

    ⬆️ upgrade to react 18

--- apps/vite-react/src/main.tsx
+++ apps/vite-react/src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './index.css'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-)
+const container = document.getElementById('root')
+//@ts-nocheck
+//@ts-ignore
+const root = createRoot(container)
+root.render(<App></App>)
